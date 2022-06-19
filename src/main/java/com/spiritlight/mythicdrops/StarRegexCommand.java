fb265ad2efdc829fcd408c5b2297ab69bdd40fb7
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-06-19 15:50:44 +0800

    Fixed RegexStar not deleting correctly

--- src/main/java/com/spiritlight/mythicdrops/StarRegexCommand.java
+++ src/main/java/com/spiritlight/mythicdrops/StarRegexCommand.java
@@ -8,13 +8,16 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.HoverEvent;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -47,15 +50,20 @@ public class StarRegexCommand extends CommandBase {
         }
         String[] args2;
         String name;
+        Pattern pattern;
         switch(args[0].toLowerCase(Locale.ROOT)) {
             case "list":
                 nullSafeMessage.sendMessage("Starred items (RegEx) to log:");
                 for(Pattern s : Main.regexStar) {
                     TextComponentString string = new TextComponentString("- " + s.pattern());
                     string.setStyle(new Style().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString("Click to remove!")))
-                            .setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/starregex remove " + s.pattern())));
+                            .setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/starregex remove " + s)));
                     nullSafeMessage.sendMessage(string);
                 }
+                break;
+            case "clear":
+                nullSafeMessage.sendMessage("Cleared regex list.");
+                Main.regexStar.clear();
                 break;
             case "add":
                 if(args.length <= 1) {
@@ -64,9 +72,10 @@ public class StarRegexCommand extends CommandBase {
                 }
                 args2 = Arrays.copyOfRange(args, 1, args.length);
                 name = String.join(" ", args2);
+                pattern = Pattern.compile(name);
                 try {
-                    Main.regexStar.add(Pattern.compile(name));
-                    nullSafeMessage.sendMessage("Added " + name + " to regex star list.");
+                    boolean b=Main.regexStar.add(pattern);
+                    nullSafeMessage.sendMessage(b ? "Added " + name + " to regex star list." : "This pattern already exists.");
                     save();
                 }  catch (PatternSyntaxException ex) {
                     ITextComponent s = new TextComponentString("The provided RegEx is invalid. Check https://regexr.com/ for RegEx (Hover for cause..)")
@@ -83,8 +92,8 @@ public class StarRegexCommand extends CommandBase {
                 args2 = Arrays.copyOfRange(args, 1, args.length);
                 name = String.join(" ", args2);
                 try {
-                    Main.regexStar.remove(Pattern.compile(name));
-                    nullSafeMessage.sendMessage("Removed " + name + " from regex star list.");
+                    boolean b=remove(name);
+                    nullSafeMessage.sendMessage(b ? "Removed " + name + " from regex star list." : "This pattern does not exist.");
                     save();
                 }  catch (PatternSyntaxException ex) {
                     ITextComponent s = new TextComponentString("The provided RegEx is invalid. Check https://regexr.com/ for RegEx (Hover for cause..)")
@@ -100,12 +109,16 @@ public class StarRegexCommand extends CommandBase {
                 }
                 args2 = Arrays.copyOfRange(args, 1, args.length);
                 name = String.join(" ", args2);
-                for(Pattern pattern : Main.regexStar) {
-                    Matcher matcher = pattern.matcher(name);
+                boolean matched = false;
+                nullSafeMessage.sendMessage("Testing " + TextFormatting.GREEN + name + TextFormatting.RESET + " with " + Main.regexStar.size() + " patterns.");
+                for(Pattern p : Main.regexStar) {
+                    Matcher matcher = p.matcher(name);
                     if(matcher.matches()) {
-                        nullSafeMessage.sendMessage("Pattern " + pattern.pattern() + " matches " + name + ".");
+                        nullSafeMessage.sendMessage("Pattern " + p.pattern() + " matches " + name + ".");
+                        matched = true;
                     }
                 }
+                nullSafeMessage.sendMessage((matched ? "Matching complete." : "Failed to match with any pattern."));
                 break;
             default:
                 nullSafeMessage.sendMessage("Invalid command. try /" + getName());
@@ -119,5 +132,14 @@ public class StarRegexCommand extends CommandBase {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean remove(String s) {
+        final Set<Pattern> patterns = new HashSet<>(Main.regexStar);
+        for(Pattern p : patterns) {
+            if(p.toString().equals(s))
+                return Main.regexStar.remove(p);
+        }
+        return false;
     }
 }
